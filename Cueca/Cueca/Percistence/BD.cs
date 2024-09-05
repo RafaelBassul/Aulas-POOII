ec5810eca8ceea70f8cf2e986286771dc9b5121e
Rafael Bassul <125609709+RafaelBassul@users.noreply.github.com>
2024-09-05 08:48:56 -0300

    Add files via upload

---
+++ Cueca/Cueca/Percistence/BD.cs
@@ -0,0 +1,18 @@
+﻿using Cueca.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cueca.Percistence
+{
+    internal class BD
+    {
+        public static List<IdeiaInovacao> mybd = new ();
+
+        public static void SalvarBD(IdeiaInovacao i) => mybd.Add(i);
+
+        public static List<IdeiaInovacao> RetomarBD() => mybd;
+    }
+}
---
+++ Cueca/Cueca/model/IdeiaInovacao.cs
@@ -0,0 +1,23 @@
+﻿using Cueca.Percistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cueca.model
+{
+    internal class IdeiaInovacao
+    {
+        public string Area { get; set; } = "";
+        public string Ideia { get; set; } = "";
+        public float Custo { get; set; } = 0;
+
+        public Boolean CadastrarIdeiaInovacao(IdeiaInovacao i)
+        {
+            BD.SalvarBD(i);
+            return true;
+        }
+
+    }
+}
